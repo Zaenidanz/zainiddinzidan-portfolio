2169b88001c1f069e22bcb1e4b6814ce948493fe
Zainiddin Zidan <zainiddinzidan62@gmail.com>
2026-05-03 14:22:17 +0700

    change quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -7,8 +7,7 @@ export default function Quote() {
     <section className="py-40 bg-black text-white flex justify-center items-center px-6">
       <div className="max-w-3xl text-center">
         <h3 className="text-2xl md:text-4xl font-medium leading-relaxed tracking-tight text-secondary mb-12">
-          "Hoping AI doesn't take over my job... but also hoping it finishes my
-          code.."
+          "It's not a bug, it's an undocumented feature."
         </h3>
         <Magnetic intensity={0.2}>
           <div className="inline-block cursor-default text-sm tracking-widest uppercase text-muted">
